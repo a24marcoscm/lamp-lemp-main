bc28d9f903341d64246e12f867b061c2881d6f1c
A24MarcosCM <a24marcoscm@iessanclemente.net>
2025-03-11 19:54:45 +0100

    Changes

--- basededatos/backup.sh
+++ basededatos/backup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-#Script para facer backup da BD
+
+. ./.env.dev # .env.dev en desenvolvemento, .env.prod en produccion, # TODO: Cambiar .env.*
+
+docker compose -f compose-lamp.dev.yml exec basededatos bash -c "mysqldump -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE" > basededatos/backup-bd.sql
+echo "Backup completado: basededatos/backup-bd.sql"
+
--- basededatos/restore.sh
+++ basededatos/restore.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-#Script para restaurar a BD
+
+. ./.env.dev # .env.dev en desenvolvemento, .env.prod en produccion, # TODO: Cambiar .env.*
+
+docker compose -f compose-lamp.dev.yml exec basededatos bash -c "mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /basededatos/backup-bd.sql"
+echo "Restauración completada desde basededatos/backup-bd.sql"
